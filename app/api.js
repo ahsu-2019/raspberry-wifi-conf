8764c0d0ba597965de3757ae28cb61ab848755c7
ahsu-2021 <59214699+ahsu-2019@users.noreply.github.com>
2021-04-06 00:15:32 -0400

    remove http client

--- app/api.js
+++ app/api.js
@@ -1,10 +1,9 @@
-var path       = require("path"),
-    util       = require("util"),
-    iwlist     = require("./iwlist"),
-    express    = require("express"),
+var path = require("path"),
+    util = require("util"),
+    express = require("express"),
     bodyParser = require('body-parser'),
-    config     = require("../config.json"),
-    http_test  = config.http_test_only;
+    config = require("../config.json"),
+    http_test = config.http_test_only;
 
 // Helper function to log errors and send a generic status "SUCCESS"
 // message to the caller
@@ -23,45 +22,29 @@ function log_error_send_success_with(success_obj, error, response) {
 /*****************************************************************************\
     Returns a function which sets up the app and our various routes.
 \*****************************************************************************/
-module.exports = function(wifi_manager, callback) {
+module.exports = function (wifi_manager, callback) {
     var app = express();
 
     // Configure the app
-    app.set("view engine", "ejs");
-    app.set("views", path.join(__dirname, "views"));
     app.set("trust proxy", true);
 
     // Setup static routes to public assets
-    app.use(express.static(path.join(__dirname, "public")));
     app.use(bodyParser.json());
 
-    // Setup HTTP routes for rendering views
-    app.get("/", function(request, response) {
-        response.render("index");
-    });
-
-    // Setup HTTP routes for various APIs we wish to implement
-    // the responses to these are typically JSON
-    app.get("/api/rescan_wifi", function(request, response) {
-        console.log("Server got /rescan_wifi");
-        iwlist(function(error, result) {
-            log_error_send_success_with(result[0], error, response);
-        });
-    });
-
-    app.post("/api/enable_wifi", function(request, response) {
+    //is this the post that awaits the response we need?
+    app.post("/api/enable_wifi", function (request, response) {
         var conn_info = {
-            wifi_ssid:      request.body.wifi_ssid,
-            wifi_passcode:  request.body.wifi_passcode,
+            wifi_ssid: request.body.wifi_ssid,
+            wifi_passcode: request.body.wifi_passcode,
         };
 
         // TODO: If wifi did not come up correctly, it should fail
         // currently we ignore ifup failures.
-        wifi_manager.enable_wifi_mode(conn_info, function(error) {
+        wifi_manager.enable_wifi_mode(conn_info, function (error) {
             if (error) {
                 console.log("Enable Wifi ERROR: " + error);
                 console.log("Attempt to re-enable AP mode");
-                wifi_manager.enable_ap_mode(config.access_point.ssid, function(error) {
+                wifi_manager.enable_ap_mode(config.access_point.ssid, function (error) {
                     console.log("... AP mode reset");
                 });
                 response.redirect("/");
--- server.js
+++ server.js
@@ -3,24 +3,10 @@ var async               = require("async"),
     dependency_manager  = require("./app/dependency_manager")(),
     config              = require("./config.json");
 
-/*****************************************************************************\
-    1. Check for dependencies
-    2. Check to see if we are connected to a wifi AP
-    3. If connected to a wifi, do nothing -> exit
-    4. Convert RPI to act as a AP (with a configurable SSID)
-    5. Host a lightweight HTTP server which allows for the user to connect and
-       configure the RPIs wifi connection. The interfaces exposed are RESTy so
-       other applications can similarly implement their own UIs around the
-       data returned.
-    6. Once the RPI is successfully configured, reset it to act as a wifi
-       device (not AP anymore), and setup its wifi network based on what the
-       user picked.
-    7. At this stage, the RPI is named, and has a valid wifi connection which
-       its bound to, reboot the pi and re-run this script on startup.
-\*****************************************************************************/
+
 async.series([
 
-    // 1. Check if we have the required dependencies installed
+
     function test_deps(next_step) {
         dependency_manager.check_deps({
             "binaries": ["dnsmasq", "hostapd", "iw"],
@@ -31,7 +17,7 @@ async.series([
         });
     },
 
-    // 2. Check if wifi is enabled / connected
+
     function test_is_wifi_enabled(next_step) {
         wifi_manager.is_wifi_enabled(function(error, result_ip) {
 			
@@ -50,7 +36,7 @@ async.series([
         });
     },
     
-    // 3. Turn RPI into an access point
+
     function enable_rpi_ap(next_step) {
         wifi_manager.enable_ap_mode(config.access_point.ssid, function(error) {
             if(error) {
@@ -62,10 +48,7 @@ async.series([
         });
     },
 
-    // 4. Host HTTP server while functioning as AP, the "api.js"
-    //    file contains all the needed logic to get a basic express
-    //    server up. It uses a small angular application which allows
-    //    us to choose the wifi of our choosing.
+
     function start_http_server(next_step) {
         console.log("\nHTTP server running...");
         require("./app/api.js")(wifi_manager, next_step);
